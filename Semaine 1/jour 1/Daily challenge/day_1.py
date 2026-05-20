1c2fdb9f75aaeea7924b3b651bed2ee1fbe21a5f
idialloaka-ai <idialloaka@gmail.com>
2026-05-20 09:06:46 +0000

    exercices

--- Semaine 1/jour 1/Daily challenge/day_1.py
+++ Semaine 1/jour 1/Daily challenge/day_1.py
@@ -1,6 +1,18 @@
 
 # Challenge 1
+"""Ask the user for a number and a length.
+Create a program that prints a list of multiples of the number until the list length reaches length.
+Examples
 
+
+
+number: 7 - length 5 ➞ [7, 14, 21, 28, 35]
+
+number: 12 - length 10 ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
+
+number: 17 - length 6 ➞ [17, 34, 51, 68, 85, 102]
+
+"""
 number = int(input("Enter a number: "))
 length = int(input("Enter the length: "))
 multiples = []
@@ -9,6 +21,20 @@ for i in range(1, length + 1):
 print(multiples)
 
 # Challenge 2
+"""Write a program that asks a string to the user, and display a new string with any duplicate consecutive letters removed.
+
+Examples
+
+
+
+user's word : "ppoeemm" ➞ "poem"
+
+user's word : "wiiiinnnnd" ➞ "wind"
+
+user's word : "ttiiitllleeee" ➞ "title"
+
+user's word : "cccccaaarrrbbonnnnn" ➞ "carbon"
+"""
 
 user_word = input("Enter a string: ")
 cleaned_word = ""
